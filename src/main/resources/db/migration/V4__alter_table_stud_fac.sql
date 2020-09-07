f0a098e9edd6cd89cab43c486c47f9744b1441aa
alice rascol <alicerphi@gmail.com>
2020-09-07 21:08:30 +0300

    removed migration 4

--- src/main/resources/db/migration/V4__alter_table_stud_fac.sql
+++
@@ -1,5 +0,0 @@
--- alter table stud_fac_table
---   add  CONSTRAINT FK_first_key FOREIGN KEY (stud_id) REFERENCES student (id);
---
--- alter table stud_fac_table
---   add  CONSTRAINT FK_second_key FOREIGN KEY (fac_id) REFERENCES faculty (id);
